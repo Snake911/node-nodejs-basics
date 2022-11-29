6678d81e6391e9f128278bcc1146aaabc9751635
Volkov Danil <snake1234567890-1@yandex.ru>
2022-11-29 15:48:20 +0300

    feat: implement function in copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    fs.mkdir(path.join(__dirname, 'files_copy'), (error) => {
+        if (error) throw new Error('FS operation failed');
+
+        fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+            if (error) throw new Error('FS operation failed');
+            
+            files.forEach(file => {
+                fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files_copy', file), (error) => {
+                    if (error) throw new Error('FS operation failed');
+                });
+            });
+        });
+    });
 };
 
 copy();
