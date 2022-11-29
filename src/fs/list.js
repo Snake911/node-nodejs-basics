5395c52b5ad4aaa7c3309e5ba86446bb56a04658
Volkov Danil <snake1234567890-1@yandex.ru>
2022-11-29 15:57:56 +0300

    feat: implement function in list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'fs';
+import * as path from 'path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const list = async () => {
-    // Write your code here 
+    readdir(path.join(__dirname, 'files'), (error, files) => {
+        if (error) throw new Error('FS operation failed');
+        
+        files.forEach(file => {
+            console.log(file);
+        });
+    }); 
 };
 
 await list();
