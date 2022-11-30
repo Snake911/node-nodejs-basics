de0c11188176f01526283f8af4eb820fc7799e3b
Volkov Danil <snake1234567890-1@yandex.ru>
2022-11-30 12:46:13 +0300

    feat: implement function in calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
-const calculateHash = async () => {
-    // Write your code here 
+import * as crypto from 'crypto';
+import { readFile } from 'fs';
+import * as path from 'path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const calculateHash = async () => {    
+    readFile(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'), 'utf8', (error, data) => {
+        if (error) throw new Error('FS operation failed');
+        console.log(crypto.createHash('sha256').update(data).digest('hex'));
+    }); 
 };
 
 await calculateHash();
