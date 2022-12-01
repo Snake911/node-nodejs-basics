cfc0935845c16b5e7688c7dc3d52102848685e28
Volkov Danil <snake1234567890-1@yandex.ru>
2022-12-01 12:28:40 +0300

    feat: implement function in streams\write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import process from 'process';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'));
+    process.stdin.on('readable', function() {
+        var chunk = process.stdin.read();
+        if (chunk !== null) {
+            writeStream.write(chunk);
+        }
+    });
 };
 
 await write();
