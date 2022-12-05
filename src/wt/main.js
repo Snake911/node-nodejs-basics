117bb03146c6b8bc80ea5d3a015c73ba11875225
Volkov Danil <snake1234567890-1@yandex.ru>
2022-12-05 15:53:56 +0300

    fix: change function in wt\main.js to promises

--- src/wt/main.js
+++ src/wt/main.js
@@ -8,23 +8,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const performCalculations = async () => {
-    const result = [];
-    const cpusLength = os.cpus().length;
-    for (let index = 0; index < cpusLength; index++) {
-        new Worker(path.join(__dirname, 'worker.js'), {
-            workerData: 10 + index,
-        }).on('message', message => {            
-            result[index] = { status: 'resolved', data: message };
-            if(result.length === cpusLength && !result.includes(undefined)) {
-                console.log(result);
-            }
-        }).on('error', () => {
-            result[index] = { status: 'error', data: null };
-            if(result.length === cpusLength && !result.includes(undefined)) {
-                console.log(result);
-            }
-        });
-    }
+    const cpus = os.cpus();
+    Promise.all(
+        cpus.map(async (_, i) => {
+            return new Promise((resolve, reject) => {
+                new Worker(path.join(__dirname, 'worker.js'), {
+                    workerData: 10 + i,
+                }).on('message', value => {
+                    resolve({ status: 'resolved', data: value });
+                }).on('error', () => {
+                    resolve({ status: 'error', data: null });
+                });
+            });            
+        })
+    ).then(res => {
+        console.log(res);
+    })
 };
 
 await performCalculations();
